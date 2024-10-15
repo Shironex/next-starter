b28ef0fb2631bce38f363096368d8ca36c5ef37a
Kacper <kacperlachowiczwp.pl@wp.pl>
2024-10-15 23:33:45 +0200

    feat: Add environment configuration and validation for client and server

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,10 @@
+import { createJiti } from 'jiti'
+import { fileURLToPath } from 'node:url'
+
+const jiti = createJiti(fileURLToPath(import.meta.url))
+await jiti.import('./src/env/server.ts', { default: true })
+await jiti.import('./src/env/client.ts', { default: true })
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
